c050ef7353019f354aae043bf25ef772f510b82f
Pavel Krivy <pavel.krivy@email.cz>
2024-12-24 22:24:59 +0100

    h

---
+++ src/register.js
@@ -0,0 +1,22 @@
+/**
+ * Register slash commands with a local run
+ */
+import { REST, Routes } from "discord.js";
+import * as commands from "./commands.js";
+import "dotenv/config";
+
+const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
+const commandsArray = Object.values(commands);
+
+try {
+  console.log("Started refreshing application (/) commands.");
+  await rest.put(Routes.applicationCommands(process.env.DISCORD_APPLICATION_ID), { body: commandsArray });
+  console.log("Successfully reloaded application (/) commands.");
+} catch (error) {
+  console.error(error);
+}
+export default {
+	async fetch(request, env, ctx) {
+		return new Response('sup nigga');
+	},
+};
